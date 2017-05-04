3b4c7bd8b3b2dbef097586420954d48938810302
Jan Van Uytven <jvanuytven@change.org>
2017-05-04 15:57:25 -0700

    Up to 'artistic tweaking'

--- src/markov_elear/generator.clj
+++ src/markov_elear/generator.clj
@@ -1,2 +1,59 @@
 (ns markov-elear.generator)
 
+(require 'clojure.set)
+
+(defn word-chain [word-transitions]
+  (reduce (fn [r t] (merge-with clojure.set/union r
+                                (let [[a b c] t]
+                                  {[a b] (if c #{c} #{})})))
+          {}
+          word-transitions))
+
+(defn text->word-chain [s]
+  (let [words (clojure.string/split s #"[\s|\n]")
+        word-transitions (partition-all 3 1 words)]
+    (word-chain word-transitions)))
+
+(defn chain->text [chain]
+  (apply str (interpose " " chain)))
+
+(defn walk-chain [prefix chain result]
+  (let [suffixes (get chain prefix)]
+    (if (empty? suffixes)
+      result
+      (let [suffix (first (shuffle suffixes))
+            new-prefix [(last prefix) suffix]
+            result-with-spaces (chain->text result)
+            result-char-count (count result-with-spaces)
+            suffix-char-count (inc (count suffix))
+            new-result-char-count (+ result-char-count suffix-char-count)]
+        (if (>= new-result-char-count 140)
+          result
+          (recur new-prefix chain (conj result suffix)))))))
+
+(defn generate-text
+  [start-phrase word-chain]
+  (let [prefix (clojure.string/split start-phrase #" ")
+        result-chain (walk-chain prefix word-chain prefix)
+        result-text (chain->text result-chain)]
+    result-text))
+
+(defn process-file [fname]
+  (text->word-chain
+   (slurp (clojure.java.io/resource fname))))
+
+(def files ["quangle-wrangle.txt" "conan.txt" "dunwich.txt" "nonsense.txt" "rime.txt"])
+(def functional-leary (apply merge-with clojure.set/union (map process-file files)))
+
+(def prefix-list ["On the" "They went" "And all" "We think"
+                  "For every" "No other" "To a" "And every"
+                  "We, too," "For his" "And the" "But the"
+                  "Are the" "The Pobble" "For the" "When we"
+                  "In the" "Yet we" "With only" "Are the"
+                  "Though the" "And when"
+                  "We sit" "And this" "No other" "With a"
+                  "And at" "What a" "Of the"
+                  "O please" "So that" "And all" "When they"
+                  "But before" "Whoso had" "And nobody" "And it's"
+                  "For any" "For example," "Also in" "In contrast"])
+
--- test/markov_elear/generator_test.clj
+++ test/markov_elear/generator_test.clj
@@ -2,6 +2,62 @@
   (:require [clojure.test :refer :all]
             [markov-elear.generator :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-word-chain
+  (testing "it produces a chain of the possible two step transitions between suffixes and prefixes"
+    (let [example '(("And" "the" "Golden")
+                    ("the" "Golden" "Grouse")
+                    ("And" "the" "Pobble")
+                    ("the" "Pobble" "who"))]
+      (is (= {["the" "Pobble"] #{"who"}
+              ["the" "Golden"] #{"Grouse"}
+              ["And" "the"] #{"Pobble" "Golden"}}
+             (word-chain example))))))
+
+(deftest test-text->word-chain
+  (testing "string with spaces and newlines"
+    (let [example "And the Golden Grouse\nAnd the Pobble who"]
+      (is (= {["who" nil] #{}
+              ["Pobble" "who"] #{}
+              ["the" "Pobble"] #{"who"}
+              ["Grouse" "And"] #{"the"}
+              ["Golden" "Grouse"] #{"And"}
+              ["the" "Golden"] #{"Grouse"}
+              ["And" "the"] #{"Pobble" "Golden"}}
+             (text->word-chain example))))))
+
+(deftest test-walk-chain
+  (let [chain {["who" nil] #{},
+               ["Pobble" "who"] #{},
+               ["the" "Pobble"] #{"who"},
+               ["Grouse" "And"] #{"the"},
+               ["Golden" "Grouse"] #{"And"},
+               ["the" "Golden"] #{"Grouse"},
+               ["And" "the"] #{"Pobble" "Golden"}}]
+    (testing "dead end"
+      (let [prefix ["the" "Pobble"]]
+        (is (= ["the" "Pobble" "who"]
+               (walk-chain prefix chain prefix)))))
+    (testing "multiple choices"
+      (with-redefs [shuffle (fn [c] c)]
+        (let [prefix ["And" "the"]]
+          (is (= ["And" "the" "Pobble" "who"]
+                 (walk-chain prefix chain prefix))))))
+    (testing "repeated chains"
+      (with-redefs [shuffle (fn [c] (reverse c))]
+        (let [prefix ["And" "the"]]
+          (is (> 140
+                 (count (apply str (walk-chain prefix chain prefix)))))
+          (is (= ["And" "the" "Golden" "Grouse" "And" "the" "Golden" "Grouse"]
+                 (take 8 (walk-chain prefix chain prefix)))))))))
+
+(deftest test-generate-text
+  (with-redefs [shuffle (fn [c] c)]
+    (let [chain {["who" nil] #{}
+                 ["Pobble" "who"] #{}
+                 ["the" "Pobble"] #{"who"}
+                 ["Grouse" "And"] #{"the"}
+                 ["Golden" "Grouse"] #{"And"}
+                 ["the" "Golden"] #{"Grouse"}
+                 ["And" "the"] #{"Pobble" "Golden"}}]
+      (is (= "the Pobble who" (generate-text "the Pobble" chain)))
+      (is (= "And the Pobble who" (generate-text "And the" chain))))))
